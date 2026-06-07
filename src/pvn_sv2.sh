9bd58c205c3ac7d27328ea26f07e407ccbffe762
Vedran Novakovic <venovako@venovako.eu>
2026-06-07 20:16:02 +0200

    further updates

--- src/pvn_sv2.sh
+++ src/pvn_sv2.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-#Usage ./pvn_sv2.sh (S|D|C|Z)
-echo '"COND2G", "UORTHO1", "VORTHO1", "SVDRES1"' > $1.csv
-../../libpvn/src/pvn_sv2.exe $1 1073741824 >> $1.csv
+#Usage ./pvn_sv2.sh
+echo '"T", "COND2G", "UORTHO1", "VORTHO1", "SVDRES1"' > pvn_sv2.csv
+for T in S D C Z
+do
+	echo -n "\"$T\"," >> pvn_sv2.csv
+	../../libpvn/src/pvn_sv2.exe $T 1073741824 >> pvn_sv2.csv
+done
+unset T
