3517f1e589294722d011262ccef14ecf7608b6c3
Vedran Novakovic <venovako@venovako.eu>
2026-06-08 22:25:25 +0200

    further updates

--- src/pvn_sv2.sh
+++ src/pvn_sv2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#Usage ./pvn_sv2.sh
+#Usage ./pvn_sv2.sh (tested with openSUSE Tumbleweed's gcc-16)
 echo '"T", "COND2G", "UORTHO1", "VORTHO1", "SVDRES1"' > pvn_sv2.csv
-for T in S D C Z
+for T in S D
 do
 	echo -n "\"$T\"," >> pvn_sv2.csv
 	../../libpvn/src/pvn_sv2.exe $T 1073741824 >> pvn_sv2.csv
