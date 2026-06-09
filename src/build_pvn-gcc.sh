21a34e6fa4d8084ab6bc4fe46e3ccf4a7a3929ee
Vedran Novakovic <venovako@venovako.eu>
2026-06-09 18:42:07 +0200

    further updates

--- src/build_pvn-gcc.sh
+++ src/build_pvn-gcc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Usage: ./build_pvn-gcc.sh on an Intel Cascadelake machine with GMP and MPFR installed as specified below
 cd ../../libpvn/src
-make COMPILER=gcc COMPILER_SUFFIX=-16 NDEBUG=3 OPENMP=0 SAFE=SV2,NRM GMP=$HOME/gnu MPFR=$HOME/gnu clean all
+make COMPILER=gcc COMPILER_SUFFIX=-16 NDEBUG=3 OPENMP=0 SAFE=DET,SV2,NRM GMP=$HOME/gnu MPFR=$HOME/gnu clean all
 cd -
