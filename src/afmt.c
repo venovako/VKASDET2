b7824d572100744efa326e8c35dab63802794e46
Vedran Novakovic <venovako@venovako.eu>
2026-06-10 22:36:36 +0200

    further updates

--- src/afmt.c
+++ src/afmt.c
@@ -7,25 +7,27 @@ int main(int argc, char* argv[])
     return EXIT_FAILURE;
   }
   const char T = toupper(*(argv[1]));
-  if (T == 'S') {
-    float s;
-    if (scanf(" %E", &s) <= 0)
-      break;
-    (void)printf("%# a\n", (double)s);
-  }
-  else if (T == 'D') {
-    double d;
-    if (scanf(" %lE", &d) <= 0)
-      break;
-    (void)printf("%# a\n", d);
-  }
-  else if (T == 'X') {
-    long double x;
-    if (scanf(" %LE", &x) <= 0)
-      break;
-    (void)printf("%# La\n", x);
-  }
-  else
-    return EXIT_FAILURE;
+  do {
+    if (T == 'S') {
+      float s;
+      if (scanf(" %E", &s) <= 0)
+        break;
+      (void)printf("%# a\n", (double)s);
+    }
+    else if (T == 'D') {
+      double d;
+      if (scanf(" %lE", &d) <= 0)
+        break;
+      (void)printf("%# a\n", d);
+    }
+    else if (T == 'X') {
+      long double x;
+      if (scanf(" %LE", &x) <= 0)
+        break;
+      (void)printf("%# La\n", x);
+    }
+    else
+      return EXIT_FAILURE;
+  } while(true);
   return EXIT_SUCCESS;
 }
