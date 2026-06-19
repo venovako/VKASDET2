802a47216db35a5e2fc545636d9f9f7c1e6b6291
Vedran Novakovic <venovako@venovako.eu>
2026-06-19 23:06:51 +0200

    further updates

--- src/bf.c
+++ src/bf.c
@@ -7,28 +7,36 @@
 
 #include "pvn.h"
 
-static float a;
+static double ad;
+static float af;
 
 static void a2()
 {
-  const float a2 = (a * a);
-  (void)printf("%#a\n", a2);
+  const double a2d = (ad * ad);
+  (void)printf("%#a ", a2d);
+  const float a2f = (af * af);
+  (void)printf("%#a\n", a2f);
 }
 
 int main()
 {
-  a = nextafterf(1.0f, 0.0f);
+  ad = nextafter(1.0, 0.0);
+  af = nextafterf(1.0f, 0.0f);
   if (fesetround(FE_DOWNWARD))
     return EXIT_FAILURE;
+  (void)printf("FE_DOWNWARD  : ");
   a2();
   if (fesetround(FE_TONEAREST))
     return EXIT_FAILURE;
+  (void)printf("FE_TONEAREST : ");
   a2();
   if (fesetround(FE_TOWARDZERO))
     return EXIT_FAILURE;
+  (void)printf("FE_TOWARDZERO: ");
   a2();
   if (fesetround(FE_UPWARD))
     return EXIT_FAILURE;
+  (void)printf("FE_UPWARD    : ");
   a2();
   return EXIT_SUCCESS;
 }
