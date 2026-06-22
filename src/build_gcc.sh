f0587637aaee4ccc6b6287d0543e28b5693f66d6
Vedran Novakovic <venovako@venovako.eu>
2026-06-22 20:31:25 +0200

    further updates

--- src/build_gcc.sh
+++ src/build_gcc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#Usage: ./build_pvn-gcc.sh on an Intel Cascadelake machine with GMP and MPFR installed as specified below
+#Usage: ./build_gcc.sh on an Intel Cascadelake machine with GMP and MPFR installed as specified below
 cd ../../libpvn/src
 make COMPILER=gcc COMPILER_SUFFIX=-16 NDEBUG=3 OPENMP=0 SAFE=DET,SV2,NRM GMP=$HOME/gnu MPFR=$HOME/gnu clean
 make COMPILER=gcc COMPILER_SUFFIX=-16 NDEBUG=3 OPENMP=0 SAFE=DET,SV2,NRM GMP=$HOME/gnu MPFR=$HOME/gnu -j
 cd -
+make clean
+make -j
--- src/build_phi.sh
+++ src/build_phi.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#Usage: ./build_pvn-phi.sh on an Intel Xeon Phi machine with GMP and MPFR installed as specified below
+#Usage: ./build_phi.sh on an Intel Xeon Phi machine with GMP and MPFR installed as specified below
 cd ../../libpvn/src
 make COMPILER=icx NATIVE=common-avx512 NDEBUG=3 OPENMP=0 SAFE=DET,SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr clean
 make COMPILER=icx NATIVE=common-avx512 NDEBUG=3 OPENMP=0 SAFE=DET,SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr -j
 cd -
+make clean
+make -j
