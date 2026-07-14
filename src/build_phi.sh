17511a14a93effa01206de83d065636307a488a4
Vedran Novakovic <venovako@venovako.eu>
2026-07-14 13:25:03 +0200

    further updates

--- src/build_phi.sh
+++ src/build_phi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Usage: ./build_phi.sh on an Intel Xeon Phi machine with GMP and MPFR installed as specified below
 cd ../../libpvn/src
-make COMPILER=icx NATIVE=COMMON-AVX512 NDEBUG=3 OPENMP=0 SAFE=DET,SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr clean
-make COMPILER=icx NATIVE=COMMON-AVX512 NDEBUG=3 OPENMP=0 SAFE=DET,SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr -j
+make COMPILER=icx NATIVE=COMMON-AVX512 NDEBUG=3 OPENMP=0 SAFE=SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr clean
+make COMPILER=icx NATIVE=COMMON-AVX512 NDEBUG=3 OPENMP=0 SAFE=SV2,NRM GMP=/opt/gmp MPFR=/opt/mpfr -j
 cd -
 make clean
 make -j
